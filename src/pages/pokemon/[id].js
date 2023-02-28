d2f91228a70bca8a70c76fdbf81f416207ba7aca
msholehw <msholehw>
2023-02-28 20:08:46 +0700

    add dynamic bg-color from props

--- src/pages/pokemon/[id].js
+++ src/pages/pokemon/[id].js
@@ -2,22 +2,33 @@ import Layout from "components/Layout";
 import Link from "next/link";
 
 export default function Pokemon({ image, pokemonDetails, pokemonSpecies }) {
-  console.log(pokemonDetails);
-  console.log(image);
-  console.log(pokemonSpecies);
+  // console.log(pokemonDetails);
+  // console.log(image);
+  console.log(pokemonSpecies.color.name);
+  // var color = pokemonSpecies?.color?.name
+  //   ? "bg-" + pokemonSpecies.color.name + "-900"
+  //   : "bg-gray-900";
+  let color = { backgroundColor: `bg-${pokemonSpecies.color.name}-500` };
+  // var color = "bg-yellow-900";
+  console.log(color);
   return (
     <Layout title='detail Pokemon'>
-      <div>
-        <div className='mx-auto grid max-w-2xl grid-cols-1 items-center gap-y-16 gap-x-8 py-24 px-4 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8'>
-          <div>
-            <img src={image} width='100' height='100'></img>
-            <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+      {/* <div style={color}> */}
+      {/* <div style={{ backgroundColor: `${pokemonSpecies.color.name}` }}> */}
+      {/* <div style={{ backgroundColor: `bg-${pokemonSpecies.color.name}-900` }}> */}
+      <div className={`bg-${pokemonSpecies.color.name}-500`}>
+        <div className='flex flex-col'>
+          <div className='hero container max-w-screen-lg mx-auto pb-10 justify-center'>
+            <div>
+              <img src={image} className='mx-auto'></img>
+            </div>
+            <h2 className='text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
               {pokemonDetails.name.toUpperCase()}
             </h2>
-            <p className='mt-4 text-gray-500'>
-              {pokemonSpecies.flavor_text_entries[6].flavor_text}
-            </p>
           </div>
+          <p className='mt-4 text-gray-500 text-justify'>
+            {pokemonSpecies.flavor_text_entries[6].flavor_text}
+          </p>
         </div>
       </div>
     </Layout>
