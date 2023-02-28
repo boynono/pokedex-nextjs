14c1b91726f4cc5a9e3b063e912967e5bd30da47
msholehw <msholehw>
2023-02-28 20:09:39 +0700

    2 add dynamic bg-color from props

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,26 +10,26 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ pokemons }) {
   return (
-    <Layout title="PokeDex">
-      <h1 className="text-4xl mb-8 text-center">NextJS Pokedex</h1>
-      <div className="bg-white">
-        <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          <h2 className="sr-only">Products</h2>
+    <Layout title='PokeDex'>
+      <h1 className='text-4xl mb-8 text-center'>NextJS Pokedex</h1>
+      <div className='bg-white'>
+        <div className='mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
+          <h2 className='sr-only'>Products</h2>
 
-          <div className="grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 xl:gap-x-8">
+          <div className='grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 xl:gap-x-8'>
             {pokemons.map((item, index) => (
-              <Link key={index} href={`pokemon/${index + 1}`} className="group">
-                <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+              <Link key={index} href={`pokemon/${index + 1}`} className='group'>
+                <div className='aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8'>
                   <img
                     src={item.image}
                     alt={item.name}
-                    className="h-full w-full object-cover object-center group-hover:opacity-75"
+                    className='h-full w-full object-cover object-center group-hover:opacity-75'
                   />
                 </div>
-                <h3 className="mt-4 text-sm text-gray-700">
+                <h3 className='mt-4 text-sm text-gray-700'>
                   {item.name.toUpperCase()}
                 </h3>
-                <p className="mt-1 text-lg font-medium text-gray-900"></p>
+                <p className='mt-1 text-lg font-medium text-gray-900'></p>
               </Link>
             ))}
           </div>
@@ -44,7 +44,7 @@ export async function getStaticProps() {
   const assetsUrl = "https://raw.githubusercontent.com";
 
   try {
-    const res = await axios.get(baseUrl + "/api/v2/pokemon?limit=10");
+    const res = await axios.get(baseUrl + "/api/v2/pokemon?limit=100");
     const pokemons = res.data.results.map((pokemon, index) => {
       const image =
         assetsUrl +
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,20 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: [
+    "text-2xl",
+    "text-3xl",
+    {
+      pattern:
+        /bg-(red|green|blue|black|brown|gray|pink|purple|white|yellow)-(100|200|300|500|900)/,
+      variants: ["lg", "hover", "focus", "lg:hover"],
+    },
   ],
   theme: {
     extend: {},
