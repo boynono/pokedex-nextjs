9b6096bfd02cfc5fa0af4086989fff3befeb1ca9
M Sholeh Wicaksono <msholehw@xl.co.id>
2023-03-03 17:09:30 +0700

    friday

--- src/pages/pokemon/[id].js
+++ src/pages/pokemon/[id].js
@@ -2,33 +2,29 @@ import Layout from "components/Layout";
 import Link from "next/link";
 
 export default function Pokemon({ image, pokemonDetails, pokemonSpecies }) {
-  // console.log(pokemonDetails);
-  // console.log(image);
-  console.log(pokemonSpecies.color.name);
-  // var color = pokemonSpecies?.color?.name
-  //   ? "bg-" + pokemonSpecies.color.name + "-900"
-  //   : "bg-gray-900";
-  let color = { backgroundColor: `bg-${pokemonSpecies.color.name}-500` };
-  // var color = "bg-yellow-900";
-  console.log(color);
+  console.log(pokemonDetails.types);
+
   return (
-    <Layout title='detail Pokemon'>
+    <Layout title="detail Pokemon">
       {/* <div style={color}> */}
       {/* <div style={{ backgroundColor: `${pokemonSpecies.color.name}` }}> */}
       {/* <div style={{ backgroundColor: `bg-${pokemonSpecies.color.name}-900` }}> */}
       <div className={`bg-${pokemonSpecies.color.name}-500`}>
-        <div className='flex flex-col'>
-          <div className='hero container max-w-screen-lg mx-auto pb-10 justify-center'>
+        <div className="flex flex-col">
+          <div className="hero container max-w-screen-lg mx-auto pb-10 justify-center">
             <div>
-              <img src={image} className='mx-auto'></img>
+              <img src={image} className="mx-auto"></img>
             </div>
-            <h2 className='text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+            <h2 className="text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
               {pokemonDetails.name.toUpperCase()}
             </h2>
           </div>
-          <p className='mt-4 text-gray-500 text-justify'>
+          <p className="mt-4 text-gray-500 text-justify">
             {pokemonSpecies.flavor_text_entries[6].flavor_text}
           </p>
+          {pokemonDetails.types.map((item, index) => (
+            <p key={index}>{item.type.name}</p>
+          ))}
         </div>
       </div>
     </Layout>
